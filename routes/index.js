72891824e5cd501e4bacebf3bd801fdd46fdd7a0
Jose Diaz <jdiaz31@Joses-iMac.fios-router.home>
2015-04-25 13:25:55 -0400

    clean up index.js

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,11 @@
-//var express = require('express');
-//var router = express.Router();
-
-/* GET home page. */
-
-//require('../models/schools');
-
 var mongoose = require('mongoose');
 var Collection = mongoose.model('School');
 
 exports.index = function(req, res){
-	//var Collection = mongoose.model('School');
-
-	//collection.find({}, {'INSTNM': 1, '_id': 1}, function(err, schools){
 	Collection.find({}, function(err, schools){
-	//collection.findOne({UNITID: '100706'}, function(err,schools){
-		console.log(schools);
 		res.render('index', {
-			title: 'List of Schools',
+			title: 'List of Colleges',
 			schools: schools
 		});
 	});
 }
-
-//mongoose.connection.close();
-var Collection = mongoose.model('School');
-
-exports.college = function(req, res){
-	console.log(req.params.id);
-	Collection.find({UNITID: req.params.id}, function(err, course){
-	//Collection.findOne({UNITID: req.param.id}, function(err, schools){
-	//collection.findOne({ _id: req.params.cid}, function(err, course){
-	//collection.find(fields, function(err, course){
-	//collection.find
-		//console.log(fields);
-		res.send(course);
-		console.log(course);
-		res.render('college',{
-			title: 'Individual Schools',
-			course: course
-		});
-	});
-}//*/
-
-//router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-//});
-
-//module.exports = router;
